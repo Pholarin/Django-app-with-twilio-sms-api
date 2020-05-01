17d5b2e64bf4d87b4f279758bdbe5f9a269d9466
folarin <folarin2010@gmail.com>
2020-05-01 02:05:51 +0100

    New features added: User can upload a spreadsheet containing recipients number

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-"# Django-app-with-twilio-sms-api" 
+# Django-app-with-twilio-sms-api
+
 A bulk sms application built with Django and Twilio sms api.
+
+This app send text message to recipients whose number are in the uploaded spreadsheet.
+
+The app only checks the first sheet in the workbook(It can be edited accordingly).
+
+env file stores the Auth Token,Phone Number and SID.
--- mysmsapp/views.py
+++ mysmsapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpRequest,HttpResponse
 from twilio.rest import Client
 from django.conf import settings
+import openpyxl
 
 # Create your views here.
 
@@ -16,21 +17,32 @@ def broadcast_sms(request):
 
     if request.method=='POST':
     
-        raw_recipients=request.POST['numbers']
-        recipients= raw_recipients.split(",")
+        excel_recipients=request.FILES['numbers']
+        try:
+            wb = openpyxl.load_workbook(excel_recipients)
+        except Exception:
+             return render(request,r'mysmsapp\index.html',{"error":"Invalid File Type"})
 
-        message_to_broadcast= request.POST['message']
+        worksheet =wb['Sheet1']
+        number_inExcel=list()
+
+        for row in worksheet.iter_rows():
+            for cell in row:
+                number_inExcel.append(cell.value)
+
+
+        message_to_broadcast = request.POST['message']
         client= Client(settings.TWILIO_ACCOUNT_SID,settings.TWILIO_AUTH_TOKEN)
 
-        for recipient in recipients:
+        for recipient in number_inExcel:
             if recipient:
                 client.messages.create(to=recipient,
                                     from_=settings.TWILIO_NUMBER,
                                     body=message_to_broadcast
                                       )
             else:
-                return HttpResponse("<h3>The numbers are not properly formated or empty</h3>",status=200)
-    return render(request,r'mysmsapp\success.html')
+                return render(request,r'mysmsapp\index.html',{"error":"Invalid Numbers"})
+        return render(request,r'mysmsapp\success.html')
 
-
+    return render(request,r'mysmsapp\index.html')
 
